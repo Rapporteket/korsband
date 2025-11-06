0f4aaa86ed1160fefdc4a4d59ee573d9ba0ee52c
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2025-11-06 12:26:18 +0100

    Endret fra rapRegTemplate til korsband (#4)

--- R/mod_abonnement.R
+++ R/mod_abonnement.R
@@ -38,7 +38,7 @@ abonnement_server <- function(id, user) {
 
   ## Subscription
   rapbase::autoReportServer(
-    id = id, registryName = "rapRegTemplate",
+    id = id, registryName = "korsband",
     type = "subscription", reports = reports, orgs = orgs, freq = "quarter", user = user
   )
 }
--- dev/run.R
+++ dev/run.R
@@ -2,4 +2,4 @@
 
 devtools::install("../rapbase", upgrade = FALSE)
 source("dev/renv.R")
-rapRegTemplate::run_app(browser = TRUE)
+korsband::run_app(browser = TRUE)
